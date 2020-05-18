fcec368a66df26ac80eff4bee505ea861b262a85
Luxiere <reaper.1998.2310@gmail.com>
2020-05-18 12:53:44 +0700

    working spiral

--- Assets/Scripts/Boss/BossSkills.cs
+++ Assets/Scripts/Boss/BossSkills.cs
@@ -11,7 +11,7 @@ public class BossSkills : MonoBehaviour
 
     private void Start()
     {
-        CircularShot(6);
+        StartCoroutine(Barrage(12, 0.5f));
     }
 
     private IEnumerator Barrage(int projectileAmount, float fireRate)
@@ -19,6 +19,10 @@ public class BossSkills : MonoBehaviour
         for(int i = 0; i < projectileAmount; i++)
         {
             SpawnBullet(new Vector2(0,1), defaultSpeed);
+            SpawnBullet(new Vector2(0, -1), defaultSpeed);
+            SpawnBullet(new Vector2(1, 0), defaultSpeed);
+            SpawnBullet(new Vector2(-1, 0), defaultSpeed);
+
             yield return new WaitForSeconds(fireRate);
         }
     }
@@ -40,6 +44,7 @@ public class BossSkills : MonoBehaviour
     {
         var instance = CircularSpawn(projectile, transform.position, degree, radius);
         instance.GetComponent<Projectile>().speed = speed;
+        instance.GetComponent<Projectile>().theta = degree;
         RewindObjectTracker.instance.AddRewindObject(instance.GetComponent<Projectile>());
     }
 
@@ -48,6 +53,9 @@ public class BossSkills : MonoBehaviour
         var instance = Instantiate(projectile, origin, Quaternion.Euler(0f, 0f, Mathf.Acos(Vector2.Dot(origin - (Vector2) transform.position, Vector2.right) / (origin - (Vector2) transform.position).magnitude)));
         instance.transform.up = origin - (Vector2)transform.position;
         instance.GetComponent<Projectile>().speed = speed;
+        Vector2 vector = origin - (Vector2)transform.position;
+        float angle = Mathf.Acos(Vector2.Dot(vector, Vector2.right) / (vector.magnitude * Vector2.right.magnitude));
+        instance.GetComponent<Projectile>().theta = origin.y > 0 ? angle * Mathf.Rad2Deg : 360 - angle * Mathf.Rad2Deg;
         RewindObjectTracker.instance.AddRewindObject(instance.GetComponent<Projectile>());
     }
 
--- Assets/Scripts/Boss/SpiralProjectile.cs
+++ Assets/Scripts/Boss/SpiralProjectile.cs
@@ -9,16 +9,21 @@ public class SpiralProjectile : Projectile, ITrackable
     private float sequence = 0;
     private Vector2 startPos = Vector2.zero;
 
+    private float c;
+    private float s;
+
     private void Start()
     {
         startPos = transform.position;
+        c = Mathf.Cos(theta * Mathf.Deg2Rad);
+        s = Mathf.Sin(theta * Mathf.Deg2Rad);
     }
 
     void Update()
     {
+
         sequence -= Time.deltaTime;
         Vector2 spiral = new Vector2(sequence * speed * Mathf.Sin(sequence), sequence * speed * Mathf.Cos(sequence));
-        transform.position = new Vector2(spiral.x * Mathf.Cos(theta * Mathf.Deg2Rad) + spiral.y * Mathf.Sin(theta * Mathf.Deg2Rad),
-            -spiral.x * Mathf.Sin(theta * Mathf.Deg2Rad) + spiral.y * Mathf.Cos(theta * Mathf.Deg2Rad)) + startPos;
+        transform.position = new Vector2(spiral.x * c - spiral.y * s, spiral.x * s + spiral.y * c) + startPos;
     }
 }
